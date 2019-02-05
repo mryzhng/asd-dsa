1c4620a46a46af76de713c9c66f84d3401d93eed
Mary Zhang <manyue.zhang@duke.edu>
2019-02-05 10:04:27 -0500

    start

---
+++ start/start.ino
@@ -0,0 +1,20 @@
+//assignment lights x5
+//reward light counter - 5th light?
+//data storage with clock
+//https://www.tigoe.com/pcomp/code/arduinowiring/1043/
+//https://github.com/tigoe/DataloggingExamples/blob/master/Citizen_Cyberscience_summit_2012/SDCardDataLogger/SDCardDataLogger.ino
+//color sensor
+
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
